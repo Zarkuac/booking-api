3eed753b02f84295d977ba817cdb5c91279b373a
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2024-09-09 16:26:23 +0400

    added services and bookings controllers

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\BusinessController;
+use App\Http\Controllers\BookingsController;
+use App\Http\Controllers\Business\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +13,12 @@ Route::post('register', [AuthController::class, 'register']);
 
 Route::apiResource('user', UserController::class);
 Route::apiResource('business', BusinessController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('service', ServiceController::class);
+    Route::apiResource('booking', BookingsController::class);
+    Route::post('update_service/{id}', [ServiceController::class, 'update']);
+});
 
 Route::post('update_business/{id}', [BusinessController::class, 'update']);
 
